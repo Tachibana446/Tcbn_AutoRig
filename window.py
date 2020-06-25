29babac121a7d1ddaccf8e9066a160fd28cda619
Tachibana446 <stinger33z@gmail.com>
2020-06-25 12:28:44 +0900

    GUIの表示に成功

--- window.py
+++ window.py
@@ -5,10 +5,11 @@ import os
 import maya.api.OpenMaya as OpenMaya
 import maya.cmds as cmds
 
-import Tcbn_AutoRig
+from Tcbn_AutoRig import Tcbn_AutoRig
 
 from PySide2 import QtWidgets
 from PySide2.QtUiTools import QUiLoader
+from maya.app.general.mayaMixin import MayaQWidgetBaseMixin
 
 
 def maya_useNewAPI():
@@ -37,19 +38,25 @@ class ShowTcbnAutoRigWindowCommand(OpenMaya.MPxCommand):
         return ShowTcbnAutoRigWindowCommand()
 
     def doIt(self, args):
-        app = QtWidgets.QApplication.instance()
+        _app = QtWidgets.QApplication.instance()
         win = SampleWindow()
         win.show()
-        app.exec_()
 
 
-class SampleWindow(QtWidgets.QMainWindow):
+class SampleWindow(MayaQWidgetBaseMixin, QtWidgets.QMainWindow):
     def __init__(self, parent=None):
         super(SampleWindow, self).__init__(parent)
 
         print(cmds.workspace(q=True, rd=True))
         print(os.getcwd())
-        self.ui = QUiLoader().load("./01.ui")
+
+        project_root = cmds.workspace(q=True, rd=True)
+        relative_path = "scripts/Tcbn_AutoRig/01.ui"
+        ui_filepath = os.path.join(project_root, relative_path)
+        if not os.path.exists(ui_filepath):
+            print ("FILE IS NOT EXISTS")
+        print("LOAD:" + ui_filepath)
+        self.ui = QUiLoader().load(ui_filepath)
         self.setCentralWidget(self.ui)
         self.ui.pushButton.clicked.connect(self.click_btn)
 
